0575bf9d908a08e9d437dc5bf3fe4b99e208615a
davlatbek <idavlatbek19@gmail.com>
2017-05-09 03:56:18 +0300

    added start.sh script

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -jar testapp.one-jar.jar extract generated_xml_from_table
+#java -jar testapp.one-jar.jar extract generated_xml_from_table
+java -jar testapp.one-jar.jar sync generated_xml_from_table.xml
